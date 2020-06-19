10a013fd61a571c5f987b0788ca5b943956a95a4
Ilya <ilya@diem.health>
2020-06-19 20:53:14 +0300

    setup docker

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN npm install --production
 COPY .env /backend/.env
 COPY . /backend
 
-CMD ["npm","start"]
+#CMD ["npm","start"]
+CMD ["node", "--experimental-modules", "-r", "dotenv/config", "./bin/www.mjs"]
 
 EXPOSE 8080
--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,28 @@
 import express from 'express';
 import logger from 'morgan';
+import session from 'express-session';
+import connectMongo from 'connect-mongo';
 import indexRouter from './routes/index.mjs';
 import apiRouter from './routes/api.mjs';
+
+const MongoStore = connectMongo(session);
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+  store: new MongoStore({
+    url: process.env.MONGODB_URI,
+    autoReconnect: true,
+  })
+}));
 
 export default app;
